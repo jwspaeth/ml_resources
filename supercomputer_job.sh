e0b42f69ea30373df884b755af15ee44c6f448cf
jwspaeth <john.w.spaeth-1@ou.edu>
2020-04-22 13:50:07 -0500

    Aligning supercomputer and local

--- supercomputer_job.sh
+++ supercomputer_job.sh
@@ -3,13 +3,13 @@
 #SBATCH --partition=normal
 #SBATCH --ntasks=1
 #SBATCH --mem=2000
-#SBATCH --output=job-output/subprocess-%j-stdout.txt
-#SBATCH --error=job-output/subprocess--%j-stderr.txt
+#SBATCH --output=job_output/subprocess-%j-stdout.txt
+#SBATCH --error=job_output/subprocess--%j-stderr.txt
 #SBATCH --time=7:00:00
 #SBATCH --job-name=subprocess-%j
 #SBATCH --mail-user=john.w.spaeth-1@ou.edu
 #SBATCH --mail-type=ALL
-#SBATCH --chdir=/home/jwspaeth/workspaces/advanced-ml/homework_4/
+#SBATCH --chdir=/home/jwspaeth/workspaces/ml_resources/
 #SBATCH --wait
 
 python3 main.py $@
